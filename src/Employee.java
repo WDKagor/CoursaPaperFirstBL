abf02986c2b33a7bbc76a19e6e8a1ae6fe8e69d4
briz1 <my1queen8my1king@gmail.com>
2024-01-29 12:14:35 +0700

    Start BL

---
+++ src/Employee.java
@@ -0,0 +1,14 @@
+public class Employee {
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String dipartment;
+    private int salary;
+    private int id;
+
+    public Employee(String lastName, String firstName, String middleName) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.middleName = middleName;
+    }
+}
