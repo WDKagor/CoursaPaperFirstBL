4b3b122e69f846617ea1230a98dcd40275323d5c
briz1 <my1queen8my1king@gmail.com>
2024-02-05 15:06:18 +0700

    GHGJ

--- src/Main.java
+++ src/Main.java
@@ -3,32 +3,68 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        Employee employee01 = new Employee("Кравченко", "Анатолий", "Сергеевич", "1", 48500);
-        Employee employee02 = new Employee("Давыдов", "Дмитрий", "Дмитриевич", "2", 81000);
-        Employee employee03 = new Employee("Березкина", "Светлана", "Алексеевна", "3", 56000);
-        Employee employee04 = new Employee("Лязгин", "Сергей", "Сергеевич", "4", 112000);
-        Employee employee05 = new Employee("Субботина", "Анна", "Дмитриевна", "5", 71500);
-        Employee employee06 = new Employee("Захарова", "Надежда", "Владимировна", "2", 79800);
-        Employee employee07 = new Employee("Токарев", "Владимир", "Афонасьевич", "1", 48500);
-        Employee employee08 = new Employee("Селезнева", "Тамара", "Генадбевна", "3", 57600);
-
         Employee[] listOfEmployees = new Employee[10];
-        listOfEmployees[0] = employee01;
-        listOfEmployees[1] = employee02;
-        listOfEmployees[2] = employee03;
-        listOfEmployees[3] = employee04;
-        listOfEmployees[4] = employee05;
-        listOfEmployees[5] = employee06;
-        listOfEmployees[6] = employee07;
-        listOfEmployees[7] = employee08;
+        {
+            listOfEmployees[0] = new Employee("Кравченко", "Анатолий", "Сергеевич", "1", 48500);
+            listOfEmployees[1] = new Employee("Давыдов", "Дмитрий", "Дмитриевич", "2", 81000);
+            listOfEmployees[2] = new Employee("Березкина", "Светлана", "Алексеевна", "3", 56000);
+            listOfEmployees[3] = new Employee("Лязгин", "Сергей", "Сергеевич", "4", 112000);
+            listOfEmployees[4] = new Employee("Субботина", "Анна", "Дмитриевна", "5", 71500);
+            listOfEmployees[5] = new Employee("Захарова", "Надежда", "Владимировна", "2", 79800);
+            listOfEmployees[6] = new Employee("Токарев", "Владимир", "Афонасьевич", "1", 48500);
+            listOfEmployees[7] = new Employee("Селезнева", "Тамара", "Генадбевна", "3", 57600);
+            listOfEmployees[8] = new Employee("Гегельман", "Захар", "Юсупович", "4", 126600);
+            listOfEmployees[9] = new Employee("Потапова", "Татьяна", "Андреевна", "1", 48500);
+        }
+        printAllEmployee(listOfEmployees);
+        int resultSalary = culculateSalary(listOfEmployees);
+        System.out.println("Фонд заработной платы: " + resultSalary + " руб.");
+        int minSalary = minSalary(listOfEmployees);
+        System.out.println("Минимальная заработная плата: " + minSalary + " руб.");
+        int maxSalary = maxSalary(listOfEmployees);
+        System.out.println("Максимальная заработная плата: " + maxSalary + " руб.");
+        int avarageSalary = avarageSalary(listOfEmployees);
+        System.out.println("Средняя заработная плата: " + avarageSalary + " руб.");
+    }
 
+    private static void printAllEmployee(Employee[] listOfEmployees) {
         for (int i = 0; i < listOfEmployees.length; i++) {
             Employee employees = listOfEmployees[i];
             System.out.println(employees);
-            }
         }
-
-
     }
+
+    private static int culculateSalary(Employee[] listOfEmployees) {
+        int resultSalary = 0;
+        for (int i = 0; i < listOfEmployees.length; i++) {
+            Employee employee = listOfEmployees[i];
+            resultSalary = resultSalary + employee.getSalary();
+                    }
+        return resultSalary;
+    }
+
+    private static int minSalary(Employee[] listOfEmployees) {
+        int minSalary = listOfEmployees[0].getSalary();
+        for (int i = 0; i < listOfEmployees.length; i++) {
+            if (minSalary > listOfEmployees[i].getSalary())
+                minSalary = listOfEmployees[i].getSalary();
+        }
+        return minSalary;
+    }
+    private static int maxSalary(Employee[] listOfEmployees) {
+        int maxSalary = listOfEmployees[0].getSalary();
+        for (int i = 0; i < listOfEmployees.length; i++) {
+            if (maxSalary < listOfEmployees[i].getSalary())
+                maxSalary = listOfEmployees[i].getSalary();
+        }
+        return maxSalary;
+    }
+
+    private static int avarageSalary(Employee[] listOfEmployees) {
+        int avarageSalary = culculateSalary(listOfEmployees)/listOfEmployees.length;
+        return avarageSalary;
+    }
+
+
 
 }
